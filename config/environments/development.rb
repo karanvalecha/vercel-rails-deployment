ae01ff94ec11377dbe44fa05ed16d51582a9d635
Karan Valecha <karan.valecha92@gmail.com>
2024-11-29 20:03:29 +0530

    upgrade to latest rails 8 app

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,8 +34,8 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  host = '0ebe1dc6d40e4a4bb06e0ca7fe138127.vfs.cloud9.us-east-2.amazonaws.com'
-  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { host: host, protocol: 'http' }
 
 
   config.action_mailer.perform_caching = false
